07e86cd6400c0175c8ad1c9a3a95d0ffd5b92a66
Inna.Pirina <inna.pirina@cas.de>
2020-12-23 22:15:03 +0100

    add Day 23 solution - Cup game, Map that points cups to following cups

---
+++ Advent_2020/src/main/java/Day23/CupGame.java
@@ -0,0 +1,17 @@
+package Day23;
+/* Created on 23.12.2020 by Inna.Pirina (mailto:Inna.Pirina@cas.de)
+ *
+ * Project: CAS Open
+ *
+ * This software is confidential and proprietary information of CAS
+ * Software AG. You shall not disclose such Confidential Information 
+ * and shall use it only in accordance with the terms of the license 
+ * agreement you entered into with CAS Software AG.
+ *//**
+* 
+*
+* @author Inna Pirina (<a href="mailto:Inna.Pirina@cas.de">Inna.Pirina@cas.de</a>)
+* @since 23.12.2020
+*/
+public class CupGame {
+}
