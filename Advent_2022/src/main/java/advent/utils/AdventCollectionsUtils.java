cfa5e285b35eb5dfcb112e4638e28d687f0e3c03
Inna.Pirina <inna.pirina@cas.de>
2022-12-04 17:09:59 +0100

    Add methods to check various intersections in lists

--- Advent_2022/src/main/java/advent/utils/AdventCollectionsUtils.java
+++ Advent_2022/src/main/java/advent/utils/AdventCollectionsUtils.java
@@ -2,6 +2,8 @@ package advent.utils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AdventCollectionsUtils {
 
@@ -13,5 +15,17 @@ public class AdventCollectionsUtils {
         return Arrays.stream(toSplit.split(delimiter))
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    public static boolean listWithinList(List<Integer> innerList, List<Integer> outerList) {
+        return outerList.contains(innerList.get(0)) && outerList.contains(innerList.get(innerList.size() - 1));
+    }
+
+    public static boolean listHasIntersection(List<Integer> firstList, List<Integer> secondList) {
+        Set<Integer> areaIntersection = firstList.stream()
+                .distinct()
+                .filter(secondList::contains)
+                .collect(Collectors.toSet());
+        return !areaIntersection.isEmpty();
     }
 }
